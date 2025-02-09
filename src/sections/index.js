5f05f61bd1908f0425e097e0ba09794bdb8f7f96
judejudo <ocomilj@gmail.com>
2025-02-09 06:42:44 +0300

    Second Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import services from "./services";
+import PopularProducts from "./PopularProducts";
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    services,
+    CustomerReviews,
+    Footer,
+    SuperQuality,
+    Subscribe
+}
